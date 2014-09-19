4eae5313be19cb0b21dad3d03486628ebd1db0d0
tianzhiyuan <tianzhiyuan126@126.com>
2014-09-19 12:07:53 +0800

    1. 增加UploadVideo接口，群发视频消息前必须调用此接口

---
+++ NWeChatInterface/JsonHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NWeChatInterface
+{
+    internal static class JsonHelper
+    {
+        internal static string WriteObject(string name, string value)
+        {
+            return string.Format("\"{0}\":\"{1}\"", name, value);
+        }
+        internal static string WriteObject(string name, int value)
+        {
+            return string.Format("\"{0}\":{1}", name, value);
+        }
+        internal static string WriteObject(string name, long value)
+        {
+            return string.Format("\"{0}\":{1}", name, value);
+        }
+        internal static string WriteStart()
+        {
+            return "{";
+        }
+        internal static string WriteEnd()
+        {
+            return "}";
+        }
+    }
+}
---
+++ NWeChatInterface/Requests/UploadVideo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NWeChatInterface.Response;
+
+namespace NWeChatInterface.Requests
+{
+    /// <summary>
+    /// 如果想群发视频消息，必须先调用此接口获取media_id
+    /// 注意接口中的media_id需通过基础支持中的上传下载多媒体文件来得到<see cref="UploadMedia"/>
+    /// </summary>
+    public class UploadVideo : IPostRequest<UploadResponse>
+    {
+        public string AccessToken { get; private set; }
+        public string MediaId { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public UploadVideo(string accessToken, string mediaId, string title, string desc)
+        {
+            this.AccessToken = accessToken;
+            this.MediaId = mediaId;
+            this.Title = title;
+            this.Description = desc;
+        }
+        public string RequestUrl
+        {
+            get
+            {
+                return string.Format("https://file.api.weixin.qq.com/cgi-bin/media/uploadvideo?access_token={0}",
+                                     this.AccessToken);
+            }
+        }
+
+        public string Data
+        {
+            get
+            {
+                return string.Format("{0}{2},{3},{4}{1}", JsonHelper.WriteStart(), JsonHelper.WriteEnd(),
+                                     JsonHelper.WriteObject("media_id", this.MediaId),
+                                     JsonHelper.WriteObject("title", this.Title),
+                                     JsonHelper.WriteObject("description", this.Description));
+            }
+        }
+    }
+}
