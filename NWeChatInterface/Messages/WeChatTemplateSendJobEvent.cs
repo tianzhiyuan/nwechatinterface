e0b81d77754ddbe8474f159c4380a6ea5d747ce2
tianzhiyuan <tianzhiyuan126@126.com>
2014-09-20 14:46:13 +0800

    1. 新增发送模版消息接口（暂未实现）和模版发送成功事件

---
+++ NWeChatInterface/Messages/WeChatTemplateSendJobEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NWeChatInterface.Messages
+{
+    /// <summary>
+    /// 模版消息送达推送
+    /// 成功时 Status=success
+    /// 失败时 Status=failed:userblock 或者 Status=failed:system failed
+    /// </summary>
+    public class WeChatTemplateSendJobEvent : WeChatEventMsg
+    {
+        public long MsgID { get; set; }
+        public CData Status { get; set; }
+    }
+}
---
+++ NWeChatInterface/Requests/SendTemplateMessage.cs
@@ -0,0 +1,36 @@
+﻿using NWeChatInterface.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace NWeChatInterface.Requests
+{
+    /// <summary>
+    /// 发送模版消息
+    /// 模板消息仅用于公众号向用户发送重要的服务通知，
+    /// 只能用于符合其要求的服务场景中，如信用卡刷卡通知，商品购买成功通知等。
+    /// 不支持广告等营销类消息以及其它所有可能对用户造成骚扰的消息。
+    /// </summary>
+    public class SendTemplateMessage : IPostRequest<SendTemplateResponse>
+    {
+        public SendTemplateMessage(string accessToken)
+        {
+            this.AccessToken = accessToken;
+        }
+        public string AccessToken { get; private set; }
+        
+
+        public string RequestUrl
+        {
+            get
+            {
+                return string.Format("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={0}",
+                                     this.AccessToken);
+            }
+        }
+
+        public string Data { get { return JsonConvert.SerializeObject(new {}); } }
+    }
+}
